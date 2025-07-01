e47a2b703d2a2fbbab97de9c57a2bdb5e7061276
Amit Kairon <kairon007@gmail.com>
2025-07-01 23:38:57 +0530

    A little better

--- lib/components/loading_animation.dart
+++ lib/components/loading_animation.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class LoadingAnimation extends StatefulWidget {
   final String userInput;
@@ -22,9 +23,85 @@ class LoadingAnimation extends StatefulWidget {
 
 class _LoadingAnimationState extends State<LoadingAnimation>
     with TickerProviderStateMixin {
-  late final AnimationController _controller;
-  String? _phrase;
+  late final AnimationController _lottieController;
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  String? _currentPhrase;
+  int _currentSceneIndex = 0;
+  Timer? _sceneTimer;
+
   final _random = Random();
+
+  late final List<Map<String, dynamic>> _scenes;
+
+  @override
+  void initState() {
+    super.initState();
+    _lottieController = AnimationController(vsync: this);
+
+    _scenes = _buildScenes();
+    _playScene(0);
+  }
+
+  List<Map<String, dynamic>> _buildScenes() {
+    final phrases = _phrases[widget.tone] ?? _phrases['reflective']!;
+
+    return [
+      {
+        'lottie': 'assets/lottie/typewriter.json',
+        'sound': 'sounds/typewriter_clunk.mp3',
+        'phrase': phrases[_random.nextInt(phrases.length)],
+        'duration': const Duration(seconds: 3),
+      },
+      {
+        'lottie': 'assets/lottie/memory_book.json',
+        'sound': 'sounds/page_flip.mp3',
+        'phrase': phrases[_random.nextInt(phrases.length)],
+        'duration': const Duration(seconds: 3),
+      },
+      {
+        'lottie': 'assets/lottie/radio.json',
+        'sound': 'sounds/piano_loop.mp3',
+        'phrase': phrases[_random.nextInt(phrases.length)],
+        'duration': const Duration(seconds: 3),
+      },
+      {
+        'lottie': 'assets/lottie/pigeon.json',
+        'sound': 'sounds/pigeon_coo.mp3',
+        'phrase': phrases[_random.nextInt(phrases.length)],
+        'duration': const Duration(seconds: 3),
+      },
+      {
+        'lottie': 'assets/lottie/wooden_table.json',
+        'sound': null,
+        'phrase': 'Almost there...',
+        'duration': const Duration(seconds: 2),
+      },
+    ];
+  }
+
+  void _playScene(int index) {
+    if (index >= _scenes.length) {
+      widget.onComplete();
+      return;
+    }
+
+    setState(() {
+      _currentSceneIndex = index;
+      _currentPhrase = _scenes[index]['phrase'];
+    });
+
+    _lottieController.reset();
+    _lottieController.duration = null; // Reset duration to allow Lottie to set it
+
+    final soundAsset = _scenes[index]['sound'];
+    if (soundAsset != null) {
+      _audioPlayer.play(AssetSource(soundAsset));
+    }
+
+    _sceneTimer = Timer(_scenes[index]['duration'], () {
+      _playScene(index + 1);
+    });
+  }
 
   final _phrases = {
     'reflective': [
@@ -60,23 +137,10 @@ class _LoadingAnimationState extends State<LoadingAnimation>
   };
 
   @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-    _phrase = _getRandomPhrase();
-    Timer(const Duration(seconds: 5), () {
-      widget.onComplete();
-    });
-  }
-
-  String _getRandomPhrase() {
-    final phraseList = _phrases[widget.tone] ?? _phrases['reflective']!;
-    return phraseList[_random.nextInt(phraseList.length)];
-  }
-
-  @override
   void dispose() {
-    _controller.dispose();
+    _lottieController.dispose();
+    _audioPlayer.dispose();
+    _sceneTimer?.cancel();
     super.dispose();
   }
 
@@ -87,13 +151,13 @@ class _LoadingAnimationState extends State<LoadingAnimation>
       body: Stack(
         children: [
           Lottie.asset(
-            'assets/lottie/wooden_table.json',
-            controller: _controller,
+            _scenes[_currentSceneIndex]['lottie'],
+            controller: _lottieController,
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
             onLoaded: (composition) {
-              _controller
+              _lottieController
                 ..duration = composition.duration
                 ..repeat();
             },
@@ -103,13 +167,14 @@ class _LoadingAnimationState extends State<LoadingAnimation>
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Lottie.asset(
-                  'assets/lottie/memory_book.json',
+                  _scenes[_currentSceneIndex]['lottie'],
                   width: 200,
                   height: 200,
                 ),
                 const SizedBox(height: 20),
                 Text(
-                  _phrase ?? '',
+                  _currentPhrase ?? '',
+                  textAlign: TextAlign.center,
                   style: const TextStyle(
                     fontSize: 22,
                     fontFamily: 'Baskerville',
